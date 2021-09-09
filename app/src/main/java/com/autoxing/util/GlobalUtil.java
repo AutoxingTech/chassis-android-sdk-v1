0a987892d4eceaf440b058226a9646c7921c5784
baimi <blp.gxm@gmail.com>
2021-09-09 14:02:29 +0800

    support 6652

--- app/src/main/java/com/autoxing/util/GlobalUtil.java
+++ app/src/main/java/com/autoxing/util/GlobalUtil.java
@@ -19,6 +19,7 @@ public class GlobalUtil {
             "tun.autoxing.com:6612",
             "tun.autoxing.com:6632",
             "tun.autoxing.com:6642",
+            "tun.autoxing.com:6652",
             "192.168.43.92:8000",
             "10.10.40.135:8000",
             "192.168.1.108:8000"));
@@ -26,6 +27,7 @@ public class GlobalUtil {
             "8a3c5e000d75fa6322abe5fe23af778c0a95a860",
             "e0dd50717770cf4dfe33666c13f9b2f77be2b84a",
             "7af638fade88cf71c6466ee49d08eb6104dc3a74",
+            "25c9a5ee941d55a68493240c965d73767c4d6e4f",
             "f547b1b87c6e8b259bced0b81da8dd48082cf169",
             "cc2cdb02f542668300c861134a880d1753e7d1d1",
             "cc2cdb02f542668300c861134a880d1753e7d1d1"));
