cc36144fc177d81a9673b3c65b34e7edf36e9c6f
baimi <blp.gxm@gmail.com>
2021-10-09 18:05:16 +0800

    tweak alert and add proto-2 and waiter-4

--- app/src/main/java/com/autoxing/util/GlobalUtil.java
+++ app/src/main/java/com/autoxing/util/GlobalUtil.java
@@ -15,19 +15,23 @@ import java.util.List;
 
 public class GlobalUtil {
 
-    public static List<String> serverDataset = new LinkedList<>(Arrays.asList("tun.autoxing.com:6622",
-            "tun.autoxing.com:6612",
-            "tun.autoxing.com:6632",
-            "tun.autoxing.com:6642",
-            "tun.autoxing.com:6652",
+    public static List<String> serverDataset = new LinkedList<>(Arrays.asList("tun.autoxing.com:6622:proto-1",
+            "tun.autoxing.com:6672:proto-2",
+            "tun.autoxing.com:6612:proto-3",
+            "tun.autoxing.com:6632:waiter-1",
+            "tun.autoxing.com:6642:waiter-2",
+            "tun.autoxing.com:6652:waiter-3",
+            "tun.autoxing.com:6662:waiter-4",
             "192.168.43.92:8000",
             "10.10.40.135:8000",
             "192.168.1.108:8000"));
     public static List<String> tokenDataset = new LinkedList<>(Arrays.asList("f547b1b87c6e8b259bced0b81da8dd48082cf169",
+            "25c9a5ee941d55a68493240c965d73767c4d6e4f",
             "8a3c5e000d75fa6322abe5fe23af778c0a95a860",
             "e0dd50717770cf4dfe33666c13f9b2f77be2b84a",
             "7af638fade88cf71c6466ee49d08eb6104dc3a74",
             "25c9a5ee941d55a68493240c965d73767c4d6e4f",
+            "eede1dba5530fac919d19a011b8d85d840af6cba",
             "f547b1b87c6e8b259bced0b81da8dd48082cf169",
             "cc2cdb02f542668300c861134a880d1753e7d1d1",
             "cc2cdb02f542668300c861134a880d1753e7d1d1"));
